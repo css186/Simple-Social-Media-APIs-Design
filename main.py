8380b01df4ff5993eb4cf1aeb8a54ffb19285196
Brian <cssrm31@gmail.com>
2023-11-30 22:08:50 +0900

    create delete api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from model.Post import Post
 
 # Create app object
@@ -15,15 +15,37 @@ def get_posts():
     return {"data": "A List of posts"}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(new_post: Post):
-    result = new_post.model_dump()
-    print(result)
-    print(type(result))
+    new_post = new_post.model_dump()
+    print(new_post.get("rating"))
+    print(new_post.get("title"))
     return new_post
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    print(id)
-    return id
+def get_post(id: int, response: Response):
+
+    post = find_post(id)
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found"
+        )
+    return {"post_detail": f"{post}"}
+
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found"
+        )
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+def find_post(id):
+    pass
