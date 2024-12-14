10790308b7f1930c4945e6526a30b920f02dc73c
css186 <cssrm31@gmail.com>
2024-12-14 17:13:55 -0500

    init project

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 
+# Create an instance of FastAPI
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
