0c7f054c098744480fd5b13f4723375fd868199c
css186 <cssrm31@gmail.com>
2024-12-15 07:18:38 -0500

    Convert return type of cursor.fetchall() from tuple to dict

--- Controllers/post_controller.py
+++ Controllers/post_controller.py
@@ -5,11 +5,5 @@ class PostController:
 
     @staticmethod
     def get_posts() -> list[PostResponse]:
-        posts = PostModel.get_posts()
-        return [
-            PostResponse(
-                id=post[0],
-                title=post[1],
-                content=post[2],
-                published=post[3],
-                created_at=post[4]) for post in posts]
+        posts = PostModel.get_posts() # return a python dictionary
+        return [PostResponse(**post) for post in posts]
--- models/post_model.py
+++ models/post_model.py
@@ -1,16 +1,15 @@
 from fastapi import HTTPException
-import psycopg
 from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
-
 from database import get_connection
 import logging
+from psycopg.rows import dict_row
 
 class PostModel:
     @staticmethod
     def get_posts():
         try:
             with get_connection() as connection:
-                with connection.cursor() as cursor:
+                with connection.cursor(row_factory=dict_row) as cursor:
                     cursor.execute("SELECT * FROM posts")
                     posts = cursor.fetchall()
                     return posts
