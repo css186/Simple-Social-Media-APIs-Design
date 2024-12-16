8266264b8c66d1079f5be79bcc154bc6cadd2b11
css186 <cssrm31@gmail.com>
2024-12-16 03:18:57 -0500

    Update CRUD operations using ORM

--- app/main.py
+++ app/main.py
@@ -1,74 +1,75 @@
-from fastapi import FastAPI, Response, HTTPException, status
-import psycopg
-import os
-from dotenv import load_dotenv
+from fastapi import FastAPI, Response, HTTPException, status, Depends
 from schemas import Post
-
+from sqlalchemy.orm import Session
+import models
+from database import SessionLocal, engine, conn, cur, get_db, Base
 
 # Create an instance of FastAPI
 app = FastAPI()
 
-
-# Just for demonstration purposes (temporary)
-load_dotenv()
-
-HOST = os.getenv("DB_HOST")
-PORT = os.getenv("DB_PORT")
-DB = os.getenv("DB_NAME")
-USER = os.getenv("DB_USER")
-PASSWORD = os.getenv("DB_PASSWORD")
-
-
-conn = psycopg.connect(f"dbname={DB} user={USER}")
-cur = conn.cursor()
-
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
-def get_posts():
-    cur.execute("""SELECT * FROM posts""")
-    posts = cur.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    # cur.execute("""SELECT * FROM posts""")
+    # posts = cur.fetchall()
     return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    sql = """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """
-    cur.execute(sql, (post.title, post.content, post.published))
-    new_post = cur.fetchone()
-    conn.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump()) # unpack the post object and create a new Post object
+    db.add(new_post) # stage the new post
+    db.commit() # commit the new post to the database
+    db.refresh(new_post) # retrieve the new post and return it
+    # sql = """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """
+    # cur.execute(sql, (post.title, post.content, post.published))
+    # new_post = cur.fetchone()
+    # conn.commit()
     return {
         "message": "Post created successfully",
         "details": new_post
     }
 
 @app.get("/posts/{post_id}")
-def get_post(post_id: int):
-    sql = """SELECT * FROM posts WHERE id = %s"""
-    cur.execute(sql, (post_id,)) # Note that the second argument must be a tuple
-    post = cur.fetchone()
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first() #.first() is more efficient than .all()
+    # sql = """SELECT * FROM posts WHERE id = %s"""
+    # cur.execute(sql, (post_id,)) # Note that the second argument must be a tuple
+    # post = cur.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
     return {"data": post}
 
 @app.delete("/posts/{post_id}")
-def delete_post(post_id: int):
-    sql = """DELETE FROM posts WHERE id = %s RETURNING *"""
-    cur.execute(sql, (post_id,))
-    deleted_post = cur.fetchone()
-    conn.commit()
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    query = db.query(models.Post).filter(models.Post.id == post_id)
+    deleted_post = query.first()
+    # sql = """DELETE FROM posts WHERE id = %s RETURNING *"""
+    # cur.execute(sql, (post_id,))
+    # deleted_post = cur.fetchone()
+    # conn.commit()
     if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+    query.delete(synchronize_session=False)
+    db.commit()
     return {"message": "Post deleted successfully", "details": deleted_post}
 
 @app.put("/posts/{post_id}")
-def update_post(post_id: int, post: Post):
-    sql = """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *"""
-    cur.execute(sql, (post.title, post.content, post.published, post_id))
-    updated_post = cur.fetchone()
-    conn.commit()
+def update_post(post_id: int, post: Post, db: Session = Depends(get_db)):
+    query = db.query(models.Post).filter(models.Post.id == post_id)
+    updated_post = query.first()
+    # sql = """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *"""
+    # cur.execute(sql, (post.title, post.content, post.published, post_id))
+    # updated_post = cur.fetchone()
+    # conn.commit()
     if not updated_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+
+    query.update(post.model_dump(), synchronize_session=False)
+    db.commit()
     return {"message": "Post updated successfully", "details": updated_post}
--- database.py
+++ database.py
@@ -1,5 +1,13 @@
 from dotenv import load_dotenv
 import os
+# Native PostgreSQL connection
+import psycopg
+
+# SQLAlchemy related imports
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import models
 
 load_dotenv()
 
@@ -12,4 +20,21 @@ PASSWORD = os.getenv("DB_PASSWORD")
 # Create a database URL
 DB_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 
+
+# Native PostgreSQL connection
+conn = psycopg.connect(f"dbname={DB} user={USER}")
+cur = conn.cursor()
+
+# SQLAlchemy connection
+# Handle database connections using SQLAlchemy
+engine = create_engine(DB_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
